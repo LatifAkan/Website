72402b920bfae6cf5a3be5341f966d7fbac8c9c2
latif <latifakan1999@gmail.com>
2024-07-10 09:08:14 +0200

    design rework

--- source.js
+++ source.js
@@ -1,3 +1,4 @@
-let button_value = document.getElementsByClassName("content_value").value;
-
-console.log(button_value);
+function button_clicked() {
+    let button_value = document.getElementsByClassName("content_value").button_value;
+    console.log(button_value);   
+}
