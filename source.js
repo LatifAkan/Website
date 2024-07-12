8bf6685aaa0f69456a250bbdfc003c9b86260ca0
latif <latifakan1999@gmail.com>
2024-07-12 08:55:20 +0200

    reworked HTML and CSS

--- source.js
+++ source.js
@@ -1,5 +1,6 @@
 function button_clicked() {
     let button_value = document.getElementsByClassName("content_value").value;
-    document.getElementById("input_field").value = document.getElementById("num_7").value;
     console.log(button_value);
+    
+    document.getElementById("input_field").value = document.getElementById("num_7").value;
 }
